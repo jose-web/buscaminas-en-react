27f7a573355cbbe2cf10088751dec5a3fb36d8b8
jose <delossantosramosjose@gmail.com>
2020-02-04 09:53:39 +0100

    Eliminados archivos innecesarios y se crea el tablero, se insertan las minas y los números cercanos a la mina marcando su posición y se muestra en la página web correctamente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tablero />
   );
+}
+class Tablero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tamanio: 10,
+      nivel: 0,
+      minas: 10
+    };
+  }
+  //⬛
+  tablero = []
+
+  generaTablero = () => {
+    this.tablero = new Array(this.state.tamanio);
+    for (let i = 0; i < this.tablero.length; i++) {
+      this.tablero[i] = new Array(this.state.tamanio);
+    }
+
+    this.rellenarTablero()
+  }
+
+  rellenarTablero = () => {
+    let minas = this.state.minas
+    while (minas !== 0) {
+
+      let vertical = Math.trunc(Math.random() * this.state.tamanio)
+      let horizontal = Math.trunc(Math.random() * this.state.tamanio)
+
+      if (this.tablero[vertical][horizontal] !== "💣") {
+        this.tablero[vertical][horizontal] = "💣"
+        minas--
+      }
+    }
+    this.rellenaPosicionMina()
+  }
+
+  rellenaPosicionMina = () => {
+    for (let i = 0; i < this.tablero.length; i++) {
+      for (let o = 0; o < this.tablero[i].length; o++) {
+        if (this.tablero[i][o] !== "💣") {
+          let minas = 0
+
+          if (this.tablero[i][o - 1] === "💣")
+            minas++
+
+          if (this.tablero[i][o + 1] === "💣")
+            minas++
+
+          if (typeof this.tablero[i - 1] != "undefined") {
+            if (this.tablero[i - 1][o] === "💣")
+              minas++
+            if (this.tablero[i - 1][o - 1] === "💣")
+              minas++
+            if (this.tablero[i - 1][o + 1] === "💣")
+              minas++
+          }
+
+          if (typeof this.tablero[i + 1] != "undefined") {
+            if (this.tablero[i + 1][o] === "💣")
+              minas++
+            if (this.tablero[i + 1][o - 1] === "💣")
+              minas++
+            if (this.tablero[i + 1][o + 1] === "💣")
+              minas++
+          }
+
+          if (minas !== 0)
+            this.tablero[i][o] = minas
+        }
+      }
+    }
+  }
+
+  muestraTablero = () => {
+    let tabla = [];
+    this.generaTablero()
+    for (let i = 0; i < this.tablero.length; i++) {
+      let casillas = []
+      for (let o = 0; o < this.tablero[i].length; o++) {
+        casillas.push(<td key={o}>{this.tablero[i][o]}</td>)
+      }
+      tabla.push(<tr key={i}>{casillas}</tr>)
+    }
+    return tabla
+  }
+
+  render() {
+    return (
+      <table id="tablero">
+        <tbody>
+          {this.muestraTablero()}
+        </tbody>
+      </table>
+
+    )
+  }
 }
 
 export default App;
