060c00dede22343d0a3d1e073a179f867f7531ef
jose <delossantosramosjose@gmail.com>
2020-03-31 22:18:52 +0200

    Se añade la página en la nueva branch gh-pages

---
+++ precache-manifest.fe88593831e7006659c1b9511f6507f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf66bfd66ecc75e883b15dcf6a410cb8",
+    "url": "/buscaminas-en-react/index.html"
+  },
+  {
+    "revision": "7bb24fd5809b5affd35e",
+    "url": "/buscaminas-en-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2b335848fc08b71dc286",
+    "url": "/buscaminas-en-react/static/css/main.0b32d6bf.chunk.css"
+  },
+  {
+    "revision": "7bb24fd5809b5affd35e",
+    "url": "/buscaminas-en-react/static/js/2.c0a5598f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/buscaminas-en-react/static/js/2.c0a5598f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b335848fc08b71dc286",
+    "url": "/buscaminas-en-react/static/js/main.4bb409a4.chunk.js"
+  },
+  {
+    "revision": "415fe0dfff88e0df490e",
+    "url": "/buscaminas-en-react/static/js/runtime-main.dfe8d6cd.js"
+  }
+]);
